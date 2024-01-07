31e7fd3a170f539a30b9db9668d2375630a9bc1c
Ben Blank <ben.blank@gmail.com>
2024-01-06 18:04:18 -0800

    [WIP] theming

--- layouts/default.11ty.ts
+++ layouts/default.11ty.ts
@@ -9,13 +9,51 @@ export default class DefaultLayout {
 
     return `
       <!doctype html>
-      <html>
+      <html lang="en">
         <head>
-          <title>${title}${section ? " — " + section : ""}</title>
+          <meta charset="utf-8">
+          <title>${title}${section ? " — " + section : ""} — There You Are</title>
+          <!--<link rel="canonical" href="TODO">-->
           <meta name="generator" content="${generator}">
-        </head>
+          <meta property="og:title" content="There You Are">
+          <meta property="og:locale" content="en_US">
+          <!--<meta name="description" content="TODO">-->
+          <!--<meta property="og:description" content="TODO">-->
+          <!--<meta property="og:url" content="TODO">-->
+          <meta property="og:site_name" content="There You Are">
+          <meta property="og:type" content="website">
+          <!--<meta name="twitter:card" content="summary">-->
+          <!--<meta property="twitter:title" content="TODO">-->
+          <!--<link type="application/atom+xml" rel="alternate" href="/blog.xml" title="There You Are">-->
 
-        <body>${content}</body>
+          <!--
+          <script type="application/ld+json">
+            {
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              "description": "TODO",
+              "headline": "There You Are",
+              "name": "There You Are",
+              "url": "https://five35.com/"
+            }
+          </script>
+          -->
+          </head>
+
+        <body>
+          <header>
+            <p>There You Are</p>
+            <!--<nav>...</nav>-->
+          </header>
+
+          <main>
+            ${content}
+          </main>
+
+          <footer>
+            <p>Some stuff here.</p>
+          </footer>
+        </body>
       </html>
     `;
   }
